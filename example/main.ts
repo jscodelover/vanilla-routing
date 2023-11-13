c9bdf555e558d2db64c0aebb97be972b29e88a3c
jscodelover <manisha.4918@gmail.com>
2023-11-13 13:03:41 +0530

    add readme.md in example

--- example/main.ts
+++ example/main.ts
@@ -1,6 +1,6 @@
 import { routeConfig } from './routeConfig';
-import { BrowserRoute, HashRoute } from '../src/index';
+import { BrowserRoute } from '../src/index';
 
 document.addEventListener('DOMContentLoaded', () => {
-  HashRoute(routeConfig);
+  BrowserRoute(routeConfig);
 });
--- example/server.js
+++ example/server.js
@@ -4,18 +4,19 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the "example" directory
-app.use(express.static(path.join('app')));
+app.use('/', express.static(path.join('app')));
+app.use('*/dist', express.static(path.join('dist')));
 
 // Serve the index.html file for all routes
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'app/index.html'), function (err) {
-		if (err) {
-			res.status(500).send(err);
-		}
-	});
+  res.sendFile(path.join(__dirname, 'app/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 // Start the server
 app.listen(3000, () => {
-	console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
